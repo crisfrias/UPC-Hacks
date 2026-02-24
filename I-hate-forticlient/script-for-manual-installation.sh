d0e874020b159f3aeb5fbec408daf614c3d92db6
gRiigRii <crisfrias80@gmail.com>
2026-02-24 21:59:59 +0100

    Some changes for the last script

--- I-hate-forticlient/script-for-manual-installation.sh
+++ I-hate-forticlient/script-for-manual-installation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# First we install the dependencies
 sudo apt update
 sudo apt install build-essential \
 		 autoconf automake pkg-config \
@@ -7,13 +8,18 @@ sudo apt install build-essential \
 		 libxml2-dev \
 		 libproxy-dev
 
+# We clone the upstream repository and change to the desired branch
 git clone https://github.com/adrienverge/openfortivpn.git
 cd openfortivpn
 git checkout v1.23.1
 
+# Execute the needed configuration
 ./autogen.sh
 ./configure
 
+# Compile and install
 make 
 sudo make install
-openfortivpn --version
+
+# Show in terminal that the installation was successfull
+echo "The installation of the openfortivpn in 1.23.1 was successfull. Watch the version with openfortivpn --version"
