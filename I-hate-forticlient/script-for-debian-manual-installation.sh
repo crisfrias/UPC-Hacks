1c774de429d3a48ca2099fc1e8f1bba4bf3397c1
crisfrias <TU_EMAIL>
2026-02-25 11:25:56 +0100

    I've removed sudo from the scripts for best practices and added a mechanism for computers that don't have bash_aliases

--- I-hate-forticlient/script-for-debian-manual-installation.sh
+++ I-hate-forticlient/script-for-debian-manual-installation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # First we install the dependencies
-sudo apt update
-sudo apt install build-essential \
-		 autoconf automake pkg-config \
-		 libssl-dev \
-		 libxml2-dev \
-		 libproxy-dev
+apt update
+apt install build-essential \
+	autoconf automake pkg-config \
+	libssl-dev \
+	libxml2-dev \
+	libproxy-dev
 
 # We clone the upstream repository and change to the desired branch
 git clone https://github.com/adrienverge/openfortivpn.git
@@ -19,7 +19,7 @@ git checkout v1.23.1
 
 # Compile and install
 make 
-sudo make install
+make install
 
 # Show in terminal that the installation was successfull
 echo "The installation of the openfortivpn in 1.23.1 was successfull. Watch the version with openfortivpn --version"
@@ -29,13 +29,24 @@ echo "What's your UPC user?"
 read UPC_USER
 
 # Now we will add the vpn-upc alias to bashrc
-ALIASES=$HOME/.bash_aliases
+ALIASES="$HOME/.bash_aliases"
+BASHRC="$HOME/.bashrc"
 
-if grep -qE '^[[:space:]]*alias[[:space:]]+vpn-upc=' "$ALIASES"; then
-	sed -i -E "s|^[[:space:]]*alias[[:space:]]+vpn-upc=.*$|alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'|" "$ALIASES"
+if [[ -e "$ALIASES" ]]; then
+  echo "Using $ALIASES"
+  FILE="$ALIASES"
 else
-	echo "alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'" >> "$ALIASES"
+  echo "$ALIASES doesn't exist, using $BASHRC"
+  FILE="$BASHRC"
+fi
+
+touch "$FILE"
+
+if grep -qE '^[[:space:]]*alias[[:space:]]+vpn-upc=' "$FILE"; then
+	sed -i -E "s|^[[:space:]]*alias[[:space:]]+vpn-upc=.*$|alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'|" "$FILE"
+else
+	echo "alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'" >> "$FILE"
 fi
 
 # Finally, in the terminal it will say that the vpn was correctly configured
-echo "Openfortivpn was correctly installed. For using it, run source ~/.bash_aliases and try to type vpn-upc after that :)"
+echo "Openfortivpn was correctly installed. For using it, run source ~/.bashrc and try to type vpn-upc after that :)"
--- I-hate-forticlient/script-for-debian13.sh
+++ I-hate-forticlient/script-for-debian13.sh
@@ -2,21 +2,32 @@
 set -euo pipefail
 
 # First we install the correct package
-sudo apt update
-sudo apt install openfortivpn
+apt update
+apt install openfortivpn
 
 # We will need the upc user for the vpn connection
 echo "What's your UPC user?"
 read UPC_USER
 
 # Now we will add the vpn-upc alias to bashrc
-ALIASES=$HOME/.bash_aliases
+ALIASES="$HOME/.bash_aliases"
+BASHRC="$HOME/.bashrc"
 
-if grep -qE '^[[:space:]]*alias[[:space:]]+vpn-upc=' "$ALIASES"; then
-	sed -i -E "s|^[[:space:]]*alias[[:space:]]+vpn-upc=.*$|alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'|" "$ALIASES"
+if [[ -e "$ALIASES" ]]; then
+  echo "Using $ALIASES"
+  FILE="$ALIASES"
 else
-	echo "alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'" >> "$ALIASES"
+  echo "$ALIASES doesn't exist, using $BASHRC"
+  FILE="$BASHRC"
+fi
+
+touch "$FILE"
+
+if grep -qE '^[[:space:]]*alias[[:space:]]+vpn-upc=' "$FILE"; then
+	sed -i -E "s|^[[:space:]]*alias[[:space:]]+vpn-upc=.*$|alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'|" "$FILE"
+else
+	echo "alias vpn-upc='sudo openfortivpn --saml-login myupclink.upc.edu:443 --username ${UPC_USER}'" >> "$FILE"
 fi
 
 # Finally, in the terminal it will say that the vpn was correctly configured
-echo "Openfortivpn was correctly installed. For using it, run source ~/.bash_aliases and try to type vpn-upc after that :)"
+echo "Openfortivpn was correctly installed. For using it, run source ~/.bashrc and try to type vpn-upc after that :)"
